d624c6c421ec0c803779a46f5cf83e83ba570d85
Jonathan Hunz <johunz99@gmail.com>
2017-11-22 19:08:38 +0100

    Make downloader read sources from dataset description file
    Until now dataset sources had to be added mannualy by using the downloader.add_to_queue() method. The downloader class now uses dataset descriptions like the extractor.

--- src/data/make_data.py
+++ src/data/make_data.py
@@ -5,19 +5,14 @@ import json
 
 # Set log level to info
 logging.basicConfig(level=logging.INFO)
-downloader = utils.Downloader("../../data/raw")
-# Training data
-downloader.add_to_queue("train_data", "http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz", "gz")
-# Training labels
-downloader.add_to_queue("train_labels", "http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz", "gz")
-
-# Download resources in queue
-downloader.download()
 
 # Load dataset description file
 dataset_description = json.load(open("./dataset.json"))
-# Create dataset extractor
-extractor = utils.Extractor(dataset_description, "../../data/raw", "../../data/processed")
+
+# Download resource
+downloader = utils.Downloader(dataset_description, "../../data/raw")
+downloader.download()
 
 # Extract resources
+extractor = utils.Extractor(dataset_description, "../../data/raw", "../../data/processed")
 extractor.extract()
--- src/data/utils/Downloader.py
+++ src/data/utils/Downloader.py
@@ -4,39 +4,39 @@ import urllib.request
 
 
 class Downloader:
-    def __init__(self, data_dir):
-        self.data_dir = data_dir
-        self.queue = []
-
-    def add_to_queue(self, name, url, file_extension):
-        self.queue.append((name, url, file_extension))
+    def __init__(self, dataset, output_dir):
+        self.dataset = dataset
+        self.output_dir = output_dir
 
     def download(self):
 
         # Create data directory if it doesn't already exists
-        if not os.path.isdir(self.data_dir):
-            os.makedirs(self.data_dir)
-            logging.debug("Created data directory (%s)", self.data_dir)
+        if not os.path.isdir(self.output_dir):
+            os.makedirs(self.output_dir)
+            logging.debug("Created data directory (%s)", self.output_dir)
 
-        # TODO: Check if the resource has already been downloaded
+        num_sources = len(self.dataset["sources"])
+        index = 0
 
-        # Iterate over all elements in the queue
-        for i in range(len(self.queue)):
+        # Read all sources in the dataset description an try to download them
+        for source in self.dataset["sources"]:
 
-            resource = self.queue[i]
-            output_path = os.path.join(self.data_dir, resource[0] + "." + resource[2])
+            # TODO: Check if the resource has already been downloaded
+
+            output_path = os.path.join(self.output_dir, source["name"])
 
             # Try to download the resource
             try:
-                logging.info("Downloading element %d of %d in queue (%s)", i + 1, len(self.queue), output_path)
-                file = urllib.request.urlopen(resource[1])
+                logging.info("Downloading element %d of %d (%s)", index + 1, num_sources, output_path)
+                file = urllib.request.urlopen(source["url"])
                 # Open output file and write content of resource to it
                 open(output_path, "wb").write(file.read())
 
             except urllib.request.HTTPError as error:
-                logging.error("HTTP Error (%d): Trying to download %s", error.code, resource[1])
+                logging.error("HTTP Error (%d): Trying to download %s", error.code, source["name"])
+
+            index += 1
 
         # Check if all resources have been downloaded
-        logging.info("Successfully downloaded resources in queue")
-        # Delete all resources from queue
-        self.queue.clear()
+        logging.info("Successfully downloaded resources")
+
